e10867a6b81cfcad20c5690ee0e013a790dfdcc2
Reinaldo Coelho <165790519+DadosCoelho@users.noreply.github.com>
2025-11-17 17:56:28 -0300

    Review of configuration parameters

--- config_tabelas.py
+++ config_tabelas.py
@@ -55,8 +55,8 @@ horarios_pick = {
 
 # ---- Parâmetros gerais de distribuição ----
 
-distribuicao_lista_max = 50   # você altera para qualquer valor
-distribuicao_numeros_max = 40  # outro exemplo
+distribuicao_lista_max = 50
+distribuicao_numeros_max = 40
 
 # Pesos relativos (somatório deve ser 1.0)
 # Isto você pode alterar à vontade!
@@ -113,22 +113,22 @@ distribuicao_numeros = gerar_distribuicao_dinamica(
 # ---- Tipos combinados (estações e eventos) ----
 class_tipo = {
     # ---- ESTAÇÕES ----
-    1: {"descricao": "Verão", "tipo": "estação", "meses": [1, 2, 3], "mult": 3.5},
-    2: {"descricao": "Outono", "tipo": "estação", "meses": [4, 5, 6], "mult": 3.2},
-    3: {"descricao": "Inverno", "tipo": "estação", "meses": [7, 8, 9], "mult": 3.8},
-    4: {"descricao": "Primavera", "tipo": "estação", "meses": [10, 11, 12], "mult": 3.4},
+    1: {"descricao": "Verão", "tipo": "estação", "meses": [1, 2, 3], "mult": 1.9},
+    2: {"descricao": "Outono", "tipo": "estação", "meses": [4, 5, 6], "mult": 1.9},
+    3: {"descricao": "Inverno", "tipo": "estação", "meses": [7, 8, 9], "mult": 1.8},
+    4: {"descricao": "Primavera", "tipo": "estação", "meses": [10, 11, 12], "mult": 1.9},
 
     # ---- EVENTOS ----
-    5: {"descricao": "Férias de Verão", "tipo": "evento", "meses": [1], "mult": 3.5},
-    6: {"descricao": "Carnaval", "tipo": "evento", "meses": [2], "mult": 3.5},
-    7: {"descricao": "Semana Santa e Páscoa", "tipo": "evento", "meses": [3], "mult": 3.5},
-    8: {"descricao": "Dia das Mães", "tipo": "evento", "meses": [5], "mult": 3.2},
-    9: {"descricao": "Festa Junina", "tipo": "evento", "meses": [6], "mult": 3.3},
-    10: {"descricao": "Dia dos Pais", "tipo": "evento", "meses": [7], "mult": 3.2},
-    11: {"descricao": "Dia das Crianças", "tipo": "evento", "meses": [10], "mult": 3.5},
-    12: {"descricao": "Halloween", "tipo": "evento", "meses": [10], "mult": 3.0},
-    13: {"descricao": "Natal", "tipo": "evento", "meses": [12], "mult": 3.8},
-    14: {"descricao": "Réveillon", "tipo": "evento", "meses": [12], "mult": 4.0},
+    5: {"descricao": "Férias de Verão", "tipo": "evento", "meses": [1], "mult": 1.5},
+    6: {"descricao": "Carnaval", "tipo": "evento", "meses": [2], "mult": 1.7},
+    7: {"descricao": "Semana Santa e Páscoa", "tipo": "evento", "meses": [3], "mult": 1.5},
+    8: {"descricao": "Dia das Mães", "tipo": "evento", "meses": [5], "mult": 1.9},
+    9: {"descricao": "Festa Junina", "tipo": "evento", "meses": [6], "mult": 1.6},
+    10: {"descricao": "Dia dos Pais", "tipo": "evento", "meses": [7], "mult": 1.5},
+    11: {"descricao": "Dia das Crianças", "tipo": "evento", "meses": [10], "mult": 1.7},
+    12: {"descricao": "Halloween", "tipo": "evento", "meses": [10], "mult": 1.5},
+    13: {"descricao": "Natal", "tipo": "evento", "meses": [12], "mult": 1.8},
+    14: {"descricao": "Réveillon", "tipo": "evento", "meses": [12], "mult": 1.7},
 }
 
 
@@ -141,8 +141,8 @@ tabela_numeros = {
     "5": {"base_chance": 1.00, "dependencias": {"8": 19.00}, "tipo": [1, 7]},
     "6": {"base_chance": 1.20, "dependencias": {"8": 18.00}, "tipo": [1, 7]},
     "7": {"base_chance": 1.10, "dependencias": {"8": 17.00}, "tipo": [1, 7]},
-    "8": {"base_chance": 2.00, "dependencias": {"5": 14.00, "6": 14.00, "7": 14.00}, "tipo": [1, 7]},
-    "9": {"base_chance": 2.50, "dependencias": {}, "tipo": [1]},
+    "8": {"base_chance": 1.00, "dependencias": {"5": 14.00, "6": 14.00, "7": 14.00}, "tipo": [1, 7]},
+    "9": {"base_chance": 1.50, "dependencias": {}, "tipo": [1]},
     "10": {"base_chance": 0.80, "dependencias": {"14": 20.00}, "tipo": [2]},
     "11": {"base_chance": 1.20, "dependencias": {"14": 19.00}, "tipo": [2]},
     "12": {"base_chance": 1.00, "dependencias": {"14": 18.00}, "tipo": [2]},
@@ -150,8 +150,8 @@ tabela_numeros = {
     "14": {"base_chance": 1.80, "dependencias": {"11": 14.00}, "tipo": [2]},
     "15": {"base_chance": 1.30, "dependencias": {"16": 15.00}, "tipo": [2, 8]},
     "16": {"base_chance": 1.60, "dependencias": {"15": 15.00}, "tipo": [2, 8]},
-    "17": {"base_chance": 2.20, "dependencias": {"18": 18.00}, "tipo": [2, 9]},
-    "18": {"base_chance": 2.00, "dependencias": {"17": 17.00}, "tipo": [2, 9]},
+    "17": {"base_chance": 1.20, "dependencias": {"18": 18.00}, "tipo": [2, 9]},
+    "18": {"base_chance": 1.00, "dependencias": {"17": 17.00}, "tipo": [2, 9]},
     "19": {"base_chance": 1.20, "dependencias": {}, "tipo": [2]},
     "20": {"base_chance": 0.80, "dependencias": {}, "tipo": [2]},
     "21": {"base_chance": 1.00, "dependencias": {}, "tipo": [2]},
@@ -162,19 +162,19 @@ tabela_numeros = {
     "26": {"base_chance": 0.80, "dependencias": {}, "tipo": [2]},
     "27": {"base_chance": 0.80, "dependencias": {}, "tipo": [3]},
     "28": {"base_chance": 1.60, "dependencias": {"29": 16.00}, "tipo": [3, 10]},
-    "29": {"base_chance": 2.20, "dependencias": {"28": 16.00, "30": 15.00}, "tipo": [3, 10]},
-    "30": {"base_chance": 2.40, "dependencias": {"29": 14.00}, "tipo": [3, 10]},
+    "29": {"base_chance": 1.20, "dependencias": {"28": 16.00, "30": 15.00}, "tipo": [3, 10]},
+    "30": {"base_chance": 1.40, "dependencias": {"29": 14.00}, "tipo": [3, 10]},
     "31": {"base_chance": 0.90, "dependencias": {}, "tipo": [3]},
     "32": {"base_chance": 0.80, "dependencias": {}, "tipo": [3]},
     "33": {"base_chance": 1.30, "dependencias": {}, "tipo": [3]},
     "34": {"base_chance": 1.00, "dependencias": {}, "tipo": [3]},
     "35": {"base_chance": 0.90, "dependencias": {}, "tipo": [3]},
     "36": {"base_chance": 1.60, "dependencias": {"37": 16.00}, "tipo": [3, 10]},
-    "37": {"base_chance": 2.20, "dependencias": {"36": 14.00}, "tipo": [3, 10]},
+    "37": {"base_chance": 1.20, "dependencias": {"36": 14.00}, "tipo": [3, 10]},
     "38": {"base_chance": 0.90, "dependencias": {}, "tipo": [3, 10]},
     "39": {"base_chance": 1.00, "dependencias": {}, "tipo": [4]},
     "40": {"base_chance": 0.80, "dependencias": {}, "tipo": [4]},
-    "41": {"base_chance": 2.00, "dependencias": {"42": 15.00}, "tipo": [4, 11]},
+    "41": {"base_chance": 1.00, "dependencias": {"42": 15.00}, "tipo": [4, 11]},
     "42": {"base_chance": 0.90, "dependencias": {"41": 14.00}, "tipo": [4, 11]},
     "43": {"base_chance": 1.30, "dependencias": {}, "tipo": [4]},
     "44": {"base_chance": 1.00, "dependencias": {}, "tipo": [4]},
@@ -182,6 +182,6 @@ tabela_numeros = {
     "46": {"base_chance": 1.40, "dependencias": {"47": 15.00}, "tipo": [4, 13]},
     "47": {"base_chance": 1.60, "dependencias": {"46": 15.00}, "tipo": [4, 13]},
     "48": {"base_chance": 0.90, "dependencias": {}, "tipo": [4, 12]},
-    "49": {"base_chance": 2.00, "dependencias": {"50": 14.00}, "tipo": [4, 14]},
+    "49": {"base_chance": 1.00, "dependencias": {"50": 14.00}, "tipo": [4, 14]},
     "50": {"base_chance": 0.90, "dependencias": {"49": 15.00}, "tipo": [4, 14]},
 }
